62aa9cdf6f32bc94e14128d20bf7334f57532536
Michelle Alejandra Díaz Figueroa <midifi26@gmail.com>
2025-09-29 10:41:54 +0200

    DEteccion de scroll para logo

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import logoIcon from '../../../assets/DrinkUPIcon(1).png';
+import logoFull from '../../../assets/DrinkUP.png';
 
-const Logo = () => {
-  return <div>Logo</div>;
+const Logo = ({isScrolled}) => {
+  return (
+    <div className="logo">
+      {isScrolled ? (
+        <img src={logoFull} alt="Logo" className="logo-icon" />
+      ) : (
+        <h1 className="logo-full">
+          DrinkUP
+          <img src={logoIcon} alt="Logo" className="logo-icon" />
+        </h1>
+      )}
+    </div>
+  );
 };
 
 export default Logo;
