51ed5147a6ad6c57077aeecde270424678fd5b89
Michelle Alejandra Díaz Figueroa <midifi26@gmail.com>
2025-10-15 14:14:23 +0200

    Añadido Buscador

--- src/components/Header/SearchBar/SearchBar.jsx
+++ src/components/Header/SearchBar/SearchBar.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-const SearchBar = () => {
-  return <div>SearchBar</div>;
+const SearchBar = ({onSearch}) => {
+  const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
+
+   useEffect(() => {
+    const handler = setTimeout(() => {
+      if (inputValue.trim() !== "") {
+        onSearch(inputValue);
+        setInputValue("");
+      }
+    }, 1000);
+
+    return () => clearTimeout(handler);
+  }, [inputValue, onSearch]);
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+   const handleBack = () => {
+    navigate("/");
+  };
+
+  return <div>
+    <input
+      type="text"
+      placeholder="Buscar cóctel..."
+      onChange={handleChange}
+      className="search-bar"
+    />
+    <button onClick={handleBack} className="back-button">Volver</button>
+  </div>;
 };
 
 export default SearchBar;
